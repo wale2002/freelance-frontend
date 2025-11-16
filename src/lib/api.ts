e4ef15f4958ccb5fe2a62c87ee5ecd4c6b3411a3
wale2002 <gemred2002@gmail.com>
2025-11-16 22:35:19 +0100

    api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosResponse } from "axios";
 
 // Base configuration
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-// const API_BASE_URL = "https://freelancebackend-u3u2.onrender.com/api";
+// const API_BASE_URL =
+//   import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = "https://freelancebackend-u3u2.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
